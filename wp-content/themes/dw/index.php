c9df5e2dad396fc523a71a2c7f103ed3f6cf8361
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-02-16 12:16:04 +0100

    chore: Add base loop (fallback)

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,27 +1,12 @@
 <?php get_header(); ?>
 
-<h1><?php the_title() ?></h1>
-<p><?= get_the_content() ?></p>
-<a href="<?= get_the_permalink() ?>" title="Lien vers ma page actuelle" target="_blank">Lien</a>
-
-<?php
-$section_title = get_field('project_title');
-$section_content = get_field('project_text');
-$section_image = get_field('project_image');
-?>
-
-<section>
-  <h2>
-    <?= $section_title ?>
-  </h2>
-  <div>
-    <?= $section_content ?>
-  </div>
-  <img src="<?= $section_image['url'] ?>"
-       alt="<?= $section_image['alt'] ?>"
-       height="<?= $section_image['height'] ?>"
-       width="<?= $section_image['width'] ?>"
-       class="">
-</section>
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+  <h1><?= get_the_title() ?></h1>
+  <p><?= get_the_excerpt() ?></p>
+  <a href="<?= get_the_permalink() ?>" title="Lien vers ma page de formation : <?= get_the_title() ?>" target="_blank">Découvrir
+    cette formation !</a>
+<?php endwhile; else: ?>
+  <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
--- wp-content/themes/dw/template-home.php
+++ wp-content/themes/dw/template-home.php
@@ -2,6 +2,8 @@
 <?php get_header(); ?>
 
 <?php get_template_part('templates/components/stage/stage'); ?>
+<?php get_template_part('templates/components/cards-formations/cards-formations'); ?>
 <?php get_template_part('templates/components/text-media/text-media'); ?>
+<?php get_template_part('templates/components/advantage/advantage'); ?>
 
 <?php get_footer(); ?>
