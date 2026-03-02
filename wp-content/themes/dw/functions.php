342a937b171fc59249b4b709c638dc343a2eb3c6
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-03-02 10:55:10 +0100

    refactor: Remove comments and posts pages

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -1,32 +1,6 @@
 <?php
 
-// Gutenberg est le nouvel éditeur de contenu propre à Wordpress
-// il ne nous intéresse pas pour l'utilisation du thème que nous
-// allons créer. On va donc le désactiver :
-
-// Disable Gutenberg on the back end.
-add_filter( 'use_block_editor_for_post', '__return_false' );
-// Disable Gutenberg for widgets.
-add_filter( 'use_widgets_block_editor', '__return_false' );
-// Disable default front-end styles.
-add_action( 'wp_enqueue_scripts', function() {
-  // Remove CSS on the front end.
-  wp_dequeue_style( 'wp-block-library' );
-  // Remove Gutenberg theme.
-  wp_dequeue_style( 'wp-block-library-theme' );
-  // Remove inline global CSS on the front end.
-  wp_dequeue_style( 'global-styles' );
-}, 20 );
-
-add_action('init', 'init_remove_support', 100);
-
-// Fonction qui permet de supprimer les Text-area de base sur les pages
-function init_remove_support(): void
-{
-  remove_post_type_support('post', 'editor');
-  remove_post_type_support('page', 'editor');
-  remove_post_type_support('product', 'editor');
-}
+include ('core/theme/configuration.php');
 
 // Déclaration des menus dans wordpress
 register_nav_menu('header', 'Le menu de navigation principal qui se trouve en haut de la page');
