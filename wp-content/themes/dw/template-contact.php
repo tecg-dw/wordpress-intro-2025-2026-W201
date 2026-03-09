f014cbabd828fb6d628592654e2a3b2ddb8a6d05
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-03-09 09:20:45 +0100

    chore: Add form style + nonce form and action form

--- wp-content/themes/dw/template-contact.php
+++ wp-content/themes/dw/template-contact.php
@@ -5,6 +5,7 @@
   <h2 class="form__title">
     Mon formulaire de contact
   </h2>
+  <!-- L'action redirige vers le fichier de base de Wordpress qui dit qui fait quoi. -->
   <form class="form" method="post" action="<?= admin_url('admin-post.php'); ?>">
     <div class="form__group">
       <label class="form__label" for="name">Nom complet *</label>
@@ -25,6 +26,13 @@
       <label class="form__label" for="message">Message *</label>
       <textarea class="form__textarea" id="message" name="message"></textarea>
     </div>
+
+    <!-- Je lui dis quelle fonction il doit lancer -->
+    <input type="hidden" name="action" value="hepl_contact_form"/>
+    <!-- On s'assure que notre requête vient bien de notre site -->
+    <input type="hidden" name="contact_nonce" value="<?= wp_create_nonce('hepl_contact_form'); ?>" />
+
+    <!-- Bouton qui va soumettre le formulaire -->
     <button class="form__button" type="submit">Soumettre le formulaire</button>
   </form>
 </section>
