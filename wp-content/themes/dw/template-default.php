65e96d86230a7a6fb4c24ba50686c34eb77f6bce
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-03-09 21:14:36 +0100

    chore: Add search css and fix template / text-media css

---
+++ wp-content/themes/dw/template-default.php
@@ -0,0 +1,6 @@
+<?php /* Template Name: Default */ ?>
+<?= get_header(); ?>
+
+<?php get_template_part('templates/components/stage/stage'); ?>
+
+<?= get_footer(); ?>
--- wp-content/themes/dw/templates/components/text-media/text-media.php
+++ wp-content/themes/dw/templates/components/text-media/text-media.php
@@ -3,31 +3,45 @@ $title = get_field('tm_title');
 $text = get_field('tm_text');
 $link = get_field('tm_link');
 $image = get_field('tm_image');
-$image_position = get_field('tm_image-position');
+$image_position = get_field('tm_image-position') ?: 'left';
 ?>
 
-<section>
-  <div>
-    <?php if (!empty($title)): ?>
-      <h2><?= $title ?></h2>
+<section class="text-media text-media--image-<?= esc_attr($image_position); ?>">
+  <div class="text-media__container">
+    <?php if (!empty($image)): ?>
+      <div class="text-media__media">
+        <img
+                class="text-media__image"
+                src="<?= esc_url($image['url']); ?>"
+                alt="<?= esc_attr($image['alt']); ?>"
+                width="<?= esc_attr($image['width']); ?>"
+                height="<?= esc_attr($image['height']); ?>"
+                loading="lazy"
+        >
+      </div>
     <?php endif; ?>
-    <?php if (!empty($text)): ?>
-      <div><?= $text ?></div>
-    <?php endif; ?>
-    <?php if (!empty($link)): ?>
-      <a title="<?= $link['title'] ?>"
-         target="<?= $link['target'] ?>"
-         href="<?= $link['url'] ?>">
-        <?= $link['title'] ?>
-      </a>
-    <?php endif; ?>
+
+    <div class="text-media__content">
+      <?php if (!empty($title)): ?>
+        <h2 class="text-media__title"><?= $title; ?></h2>
+      <?php endif; ?>
+
+      <?php if (!empty($text)): ?>
+        <div class="text-media__text">
+          <?= $text; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (!empty($link)): ?>
+        <a
+                class="text-media__button"
+                title="<?= esc_attr($link['title']); ?>"
+                target="<?= esc_attr($link['target']); ?>"
+                href="<?= esc_url($link['url']); ?>"
+        >
+          <?= esc_html($link['title']); ?>
+        </a>
+      <?php endif; ?>
+    </div>
   </div>
-  <?php if (!empty($image)): ?>
-    <img class="text-media__image text-media__image--<?= $image_position ?>"
-         src="<?= $image['url'] ?>"
-         alt="<?= $image['alt'] ?>"
-         width="<?= $image['width'] ?>"
-         height="<?= $image['height'] ?>"
-  >
-  <?php endif; ?>
 </section>
