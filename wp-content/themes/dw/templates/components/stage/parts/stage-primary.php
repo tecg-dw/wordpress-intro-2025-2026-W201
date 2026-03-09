146c5532b999f348a3420c515e3b0dc5e31545f5
Dylan Jacquet <dylanjacquet235@gmail.com>
2026-03-09 21:13:48 +0100

    feat: Add stage css

--- wp-content/themes/dw/templates/components/stage/parts/stage-primary.php
+++ wp-content/themes/dw/templates/components/stage/parts/stage-primary.php
@@ -3,21 +3,32 @@ $title = get_field('stage_title');
 $text = get_field('stage_text');
 $link = get_field('stage_link');
 $image = get_field('stage_image');
-
 ?>
 
-<div>
-  <div>
-    <h1><?= $title ?></h1>
-    <div>
-      <?= $text ?>
+<section class="stage">
+  <div class="stage__container">
+    <div class="stage__content">
+      <h2 class="stage__title">
+        <?= $title ?>
+      </h2>
+      <div class="stage__text">
+        <?= $text ?>
+      </div>
+      <?php if ($link): ?>
+        <a class="stage__button" href="<?= $link['url'] ?>" title="<?= $link['title'] ?>">
+          <?= $link['title'] ?>
+        </a>
+      <?php endif; ?>
     </div>
-    <a href="<?= $link['url'] ?>" title="<?= $link['title'] ?>">
-      <?= $link['title'] ?>
-    </a>
+    <?php if ($image): ?>
+      <div class="stage__media">
+        <img class="stage__image"
+             src="<?= $image['url'] ?>"
+             alt="<?= $image['alt'] ?>"
+             width="<?= $image['width'] ?>"
+             height="<?= $image['height'] ?>"
+             loading="lazy">
+      </div>
+    <?php endif; ?>
   </div>
-  <img src="<?= $image['url'] ?>"
-       alt="<?= $image['alt'] ?>"
-       width="<?= $image['width'] ?>"
-       height="<?= $image['height'] ?>">
-</div>
+</section>
